e34e608c83e30701237d4c362bcf6aec420def9c
GabrielF9 <gabrielf.dev16@gmail.com>
2020-10-18 15:52:19 -0300

    up: update quase

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 window.addEventListener('load', () => {
-  const META_1 = 500000;
-  const PERC_META_1 = calcPercent(META_1);
-  const META_2 = 600000;
-  const PERC_META_2 = calcPercent(META_2);
-  const META_3 = 700000;
-  const PERC_META_3 = calcPercent(META_3);
-  const META_4 = 800000;
-  const PERC_META_4 = calcPercent(META_4);
-  const META_5 = 1000000;
-  const PERC_META_5 = calcPercent(META_5);
-  const META_6 = 1200000;
-  const PERC_META_6 = calcPercent(META_6);
+  const META = 1200000;
 
   axios.get('https://api.catarse.me/project_details?project_id=eq.122021')
     .then((projectRes) => {
@@ -34,6 +23,7 @@ window.addEventListener('load', () => {
 
           let total = pledged + waiting;
 
+          document.getElementById('progressbar-wait').style.width = `${calcPercent(waiting, META)}%`;
           document.getElementById('waiting').innerHTML = `${formatMoney(waiting)}`;
           document.getElementById('total-value').innerHTML = `${formatMoney(total)}`;
         })
