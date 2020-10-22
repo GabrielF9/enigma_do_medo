346e64bebe9cc2fa64c6d08c58b77cca40aafa7f
GabrielF9 <gabrielf.dev16@gmail.com>
2020-10-22 15:46:18 -0300

    up: bateu 2kk

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-  const META = 1200000;
+  const META = 3000000;
 
   axios.get('https://api.catarse.me/project_details?project_id=eq.122021')
     .then((projectRes) => {
@@ -35,6 +35,43 @@ window.addEventListener('load', () => {
     .catch((error) => {
       console.log(error);
     });
+
+  setInterval(() => {
+    axios.get('https://api.catarse.me/project_details?project_id=eq.122021')
+      .then((projectRes) => {
+        let data = projectRes.data[0];
+        let pledged = data.pledged;
+
+        document.getElementById('progressbar-conf').style.width = `${calcPercent(pledged)}%`;
+        document.getElementById('pledged').innerHTML = `${formatMoney(pledged)}`;
+
+        axios.get('https://api.catarse.me/reward_details?project_id=eq.122021')
+          .then((detailsRes) => {
+            let data = detailsRes.data;
+            let waiting = 0;
+
+            data.forEach((d) => {
+              let minimum = d.minimum_value;
+              let wait = d.waiting_payment_count;
+
+              waiting += minimum * wait;
+            });
+
+            let total = pledged + waiting;
+
+            document.getElementById('progressbar-wait').style.width = pledged >= META ? '0%' : `${(waiting + pledged) >= META ? calcPercent(META - pledged) : calcPercent(waiting)}%`;
+            document.getElementById('waiting').innerHTML = `${formatMoney(waiting)}`;
+            document.getElementById('total-value').innerHTML = `${formatMoney(total)}`;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, 10000);
 });
 
 function formatMoney(number = 0, prefix = true) {
@@ -54,7 +91,7 @@ function formatMoney(number = 0, prefix = true) {
   }
 }
 
-function calcPercent(number = 0, total = 1200000) {
+function calcPercent(number = 0, total = 3000000) {
   return (number * 100) / total;
 }
 
